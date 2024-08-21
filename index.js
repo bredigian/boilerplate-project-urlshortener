129df446f44e1b4ed3bc3122ae09738693b0e09b
Gianluca Bredice <bredicegian@gmail.com>
2024-08-21 20:49:30 -0300

    fix: body parser

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const URL = require("url").URL
 const port = process.env.PORT || 3000
 
 app.use(cors())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use("/public", express.static(`${process.cwd()}/public`))
@@ -17,21 +18,16 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html")
 })
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" })
-})
-
 const URL_SHORT_LINKS = []
 
 app.post("/api/shorturl", function (req, res) {
-  let { url } = req.body
+  let url = req.body.url
   try {
     url = new URL(url)
   } catch (error) {
-    res.json({ error: "Invalid url" })
+    console.error(error)
+    return res.json({ error: "Invalid url" })
   }
-  console.log(url)
   dns.lookup(url.hostname, function (err) {
     if (err) res.json({ error: "Invalid url" })
     else {
